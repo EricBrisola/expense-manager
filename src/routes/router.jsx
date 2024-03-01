c029b6779b1175a46074fff2edea9729ef060990
unknown <eric.coca13@gmail.com>
2024-03-01 17:56:19 -0300

    chore: changed routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,23 +3,30 @@ import Home from "../pages/Home";
 import Today from "../pages/Today";
 import LastSevenDays from "../pages/LastSevenDays";
 import LastThirtyDays from "../pages/LastThirtyDays";
+import Dashboard from "../components/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/today",
-    element: <Today />,
-  },
-  {
-    path: "/last-seven-days",
-    element: <LastSevenDays />,
-  },
-  {
-    path: "/last-thirty-days",
-    element: <LastThirtyDays />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "today",
+        element: <Today />,
+      },
+      {
+        path: "last-seven-days",
+        element: <LastSevenDays />,
+      },
+      {
+        path: "last-thirty-days",
+        element: <LastThirtyDays />,
+      },
+    ],
   },
 ]);
 
