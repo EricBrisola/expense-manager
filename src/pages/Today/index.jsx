4ae00ca12ee98310c128406c72b029cec8c8cdfa
unknown <eric.coca13@gmail.com>
2024-03-13 02:02:14 -0300

    Chore: started building todays page

--- src/pages/Today/index.jsx
+++ src/pages/Today/index.jsx
@@ -1,26 +1,46 @@
 //import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { ExpensesContext } from "../../contexts/ExpensesContext";
+import dayjs from "dayjs";
 
 function Today() {
-  const { allExpenses } = useContext(ExpensesContext);
-  console.log(allExpenses);
+  const { allExpenses, dailyTotal } = useContext(ExpensesContext);
+  //console.log(allExpenses);
 
   return (
-    <section className=" flex flex-1 bg-purple-400 text-white">
-      <p>Gastos:</p>
-      {allExpenses.length >= 1 ? (
-        allExpenses.map((expense) => {
-          return (
-            <article key={expense.id}>
-              <p>{expense.title}</p>
-              <p>R${expense.value}</p>
-            </article>
-          );
-        })
-      ) : (
-        <p>Sem nenhum gasto hoje</p>
-      )}
+    <section className=" flex flex-1 flex-col items-center bg-purple-400 text-white">
+      <article className="m-2">
+        <h1 className="text-3xl">{dayjs().format("DD/MM/YYYY")}</h1>
+      </article>
+      <article className="flex flex-wrap justify-center gap-4 py-4">
+        {allExpenses.length >= 1 ? (
+          allExpenses.map((expense) => {
+            return (
+              <article
+                key={expense.id}
+                className="flex h-48 w-48 flex-col items-center justify-around  rounded-md bg-purple-300 p-3 shadow-md duration-200 hover:scale-105"
+              >
+                <p className="text-xl font-bold">{expense.title}</p>
+                <p className="text-md font-medium">R${expense.value}</p>
+                <div className="flex w-full justify-center gap-5">
+                  <button className="h-6 w-14 rounded-md bg-green-700 px-1">
+                    Edit
+                  </button>
+                  <button className="h-6 w-14 rounded-md bg-red-800 px-1">
+                    Delete
+                  </button>
+                </div>
+              </article>
+            );
+          })
+        ) : (
+          <p>Sem nenhum gasto hoje</p>
+        )}
+      </article>
+      <article className="h-25 m-2 flex w-60 flex-col items-center gap-3 rounded-md bg-purple-300 p-2">
+        <p className="text-2xl">Gasto diário</p>
+        <p className="text-xl">R${dailyTotal}</p>
+      </article>
     </section>
   );
 }
