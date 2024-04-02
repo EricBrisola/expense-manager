7e7f0bc9dc4ca7220096d46e95d54182afed7573
EricBrisola <eric.coca13@gmail.com>
2024-04-01 21:45:15 -0300

    style: changed form style

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -8,25 +8,34 @@ const Form = () => {
   //console.log(allExpenses);
 
   return (
-    <section className="flex flex-1 items-center justify-center bg-purple-400 ">
-      <article className="flex h-96 w-96 items-center justify-center rounded-md bg-purple-300">
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <label htmlFor="new-expense-title-input">
-            <p className="text-white">Title</p>
+    <section className="flex flex-1 flex-col items-center justify-center gap-6 bg-purple-400">
+      <p className="text-xl text-white">Save your new expense</p>
+      <article className="flex h-80 w-80 items-center justify-center rounded-md bg-gray-200">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+          <label
+            htmlFor="new-expense-title-input"
+            className="flex flex-col gap-3"
+          >
+            <p className="text-lg font-semibold">Title</p>
             <input
               type="text"
               name="title"
+              className="h-10 w-56 rounded-sm border-[1px] border-purple-400 bg-transparent p-3 outline-none"
               id="new-expense-title-input"
               value={expense.title}
               onChange={handleExpenseInputs}
               required
             />
           </label>
-          <label htmlFor="new-expense-value-input">
-            <p className="text-white">Value</p>
+          <label
+            htmlFor="new-expense-value-input"
+            className="flex flex-col gap-3"
+          >
+            <p className="text-lg font-semibold">Value</p>
             <input
-              type="number"
+              type="text"
               name="value"
+              className="h-10 w-56 rounded-sm border-[1px] border-purple-400 bg-transparent p-3 outline-none hover:appearance-none"
               id="new-expense-value-input"
               value={expense.value}
               onChange={handleExpenseInputs}
