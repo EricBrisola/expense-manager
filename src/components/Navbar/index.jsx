276a9947f6227330720661bea8307b3186b9de4d
EricBrisola <eric.coca13@gmail.com>
2024-04-01 21:33:48 -0300

    refactor: added an article element to group nav buttons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,27 +2,31 @@ import NavbarBtn from "../NavbarBtn";
 
 const Navbar = () => {
   const navButtonStyle =
-    "text-white text-base flex flex-2 items-center bg-purple-200 p-2 border-l-2 border-purple-100 tracking-wide";
+    "text-white text-base flex flex-2 items-center tracking-wide hover:text-purple-500 duration-200";
   return (
-    <nav className="w-full flex">
+    <nav className="flex w-full bg-purple-200 px-6 py-3">
       <NavbarBtn
         textContent={"Expense manager"}
         path={"/"}
-        style={
-          "text-white text-2xl flex flex-1 tracking-wide bg-purple-200 p-2"
-        }
+        style={"text-white text-2xl flex flex-1 tracking-wide"}
       />
-      <NavbarBtn textContent={"Today"} path={"today"} style={navButtonStyle} />
-      <NavbarBtn
-        textContent={"Last 7 days"}
-        path={"last-seven-days"}
-        style={navButtonStyle}
-      />
-      <NavbarBtn
-        textContent={"Last 30 days"}
-        path={"last-thirty-days"}
-        style={navButtonStyle}
-      />
+      <article className="flex gap-6">
+        <NavbarBtn
+          textContent={"Today"}
+          path={"today"}
+          style={navButtonStyle}
+        />
+        <NavbarBtn
+          textContent={"Last 7 days"}
+          path={"last-seven-days"}
+          style={navButtonStyle}
+        />
+        <NavbarBtn
+          textContent={"Last 30 days"}
+          path={"last-thirty-days"}
+          style={navButtonStyle}
+        />
+      </article>
     </nav>
   );
 };
