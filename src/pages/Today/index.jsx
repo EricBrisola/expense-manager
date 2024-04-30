87c8d4373cb9f71527e1b5a01ec49071148dad66
EricBrisola <eric.coca13@gmail.com>
2024-04-29 23:27:22 -0300

    style: changed colors pallete

--- src/pages/Today/index.jsx
+++ src/pages/Today/index.jsx
@@ -13,9 +13,9 @@ function Today() {
         <h1 className="text-3xl">{dayjs().format("DD/MM/YYYY")}</h1>
       </article>
       {dailyExpenses.length >= 1 && (
-        <article className="h-25 m-2 flex w-60 flex-col items-center gap-3 rounded-md bg-purple-300 p-2">
+        <article className="h-25 m-2 flex w-60 flex-col items-center gap-3 rounded-md bg-purple-950 p-2">
           <p className="text-2xl">Gasto diário</p>
-          <p className="text-xl">R${dailyTotal}</p>
+          <p className="text-xl">R${dailyTotal.toFixed(2)}</p>
         </article>
       )}
       <article className="m-4 grid grid-cols-7 gap-4">
@@ -24,16 +24,16 @@ function Today() {
             return (
               <article
                 key={expense.id}
-                className="flex h-48 w-48 flex-col items-center justify-between rounded-md bg-purple-300 shadow-md duration-200 hover:scale-105"
+                className="flex h-48 w-48 flex-col items-center justify-between rounded-md bg-[#260D39] shadow-lg duration-200 hover:scale-105"
               >
-                <p className="w-full rounded-t-md bg-purple-200 p-1 text-center text-xl font-bold">
+                <p className="w-full rounded-t-md bg-purple-950 p-1 text-center text-xl font-bold">
                   {expense.title[0].toUpperCase() +
                     expense.title.slice(1, expense.title.length)}
                 </p>
                 <p className="text-md text-2xl font-medium">
                   R${expense.value}
                 </p>
-                <div className="flex w-full justify-center  gap-5 rounded-b-md bg-purple-200 py-2">
+                <div className="flex w-full justify-center  gap-5 rounded-b-md bg-purple-950 py-2">
                   <button className="h-6 w-14 rounded-md bg-green-700 px-1">
                     Edit
                   </button>
@@ -48,7 +48,9 @@ function Today() {
             );
           })
         ) : (
-          <p>Sem nenhum gasto hoje</p>
+          <p className="col-span-7 text-2xl font-normal">
+            No expenses made today yet
+          </p>
         )}
       </article>
     </section>
