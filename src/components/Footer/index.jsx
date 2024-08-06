d1e48b91f06f40a74d3be93ee67ed3fb9b063944
EricBrisola <eric.coca13@gmail.com>
2024-08-06 20:13:54 -0300

    chore: removed footer

--- src/components/Footer/index.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="flex justify-center bg-[#645cff] pb-0.5 text-lg tracking-wide text-white">
-      Desenvolvido por Eric Brisola
-    </footer>
-  );
-};
-export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
     <div className=" flex h-screen flex-col">
       <Navbar />
       <Outlet />
-      <Footer />
     </div>
   );
 };
