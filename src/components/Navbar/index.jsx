422321b4c01187257d0c2376bf95d35efca6eb44
EricBrisola <eric.coca13@gmail.com>
2024-08-29 00:39:11 -0300

    feat: added links to access login and signup pages

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import NavbarBtn from "../NavbarBtn";
 
 const Navbar = () => {
+  //TODO: ver logica de como mudar o botao de logar para deslogar
   const navButtonStyle =
     "text-white text-base flex flex-2 items-center tracking-wide hover:bg-[#cfe8ff] duration-200 p-2 rounded-md hover:text-[#617d98]";
   return (
@@ -10,7 +11,7 @@ const Navbar = () => {
         path={"/"}
         style={"text-white text-2xl flex flex-1 tracking-wide"}
       />
-      <article className="flex gap-6">
+      <article className="flex flex-1 justify-center gap-6">
         <NavbarBtn textContent={"Hoje"} path={"today"} style={navButtonStyle} />
         <NavbarBtn
           textContent={"Semanal"}
@@ -25,6 +26,18 @@ const Navbar = () => {
         <NavbarBtn
           textContent={"Todos gastos"}
           path={"all-expenses"}
+          style={navButtonStyle}
+        />
+      </article>
+      <article className="flex flex-1 justify-end">
+        <NavbarBtn
+          textContent={"Login"}
+          path={"login"}
+          style={navButtonStyle}
+        />
+        <NavbarBtn
+          textContent={"Cadastrar"}
+          path={"sign-up"}
           style={navButtonStyle}
         />
       </article>
